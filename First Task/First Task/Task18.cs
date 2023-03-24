9a826f8aa164c7d3c41cfff8ede7acb318351b67
RVstalker <127148738+RVstalker@users.noreply.github.com>
2023-03-24 03:17:26 +0300

    24/03/23

---
+++ First Task/First Task/Task18.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace First_Task
+{
+    public class Task18
+    {
+        // Дана строка, посчитать количество слов, которые начинаются на букву "K".
+
+        public Task18(string str)
+        {
+            var counter = 0;
+            Console.WriteLine($"Your string is: {str}");
+
+            if (str[0] == 'K')
+                counter += 1;
+
+            var str1 = str.Replace(" K", "");
+            counter += (str.Length - str1.Length) / 2;
+            Console.WriteLine($"Result is: {counter}");
+            Console.ReadLine();
+        }
+    }
+}
